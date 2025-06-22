12a4ea075c85d9d91168dc758e1c5ff8822f893c
Riskiii098 <151342648+Riskiii098@users.noreply.github.com>
2025-06-22 17:02:34 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-if(!isset($_SESSION['admin'])) {
-    header('Location: login.php'); exit;
+if (!isset($_SESSION['admin'])) {
+    header('Location: login.php');
+    exit;
 }
 ?>
 <!DOCTYPE html>
