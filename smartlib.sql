80d14cdee253ffef33881553d1aba5d64f099eb1
Riskiii098 <151342648+Riskiii098@users.noreply.github.com>
2025-06-22 17:04:43 +0700

    Update smartlib.sql

--- smartlib.sql
+++ smartlib.sql
@@ -3,24 +3,24 @@ CREATE DATABASE IF NOT EXISTS smartlib CHARACTER SET utf8mb4 COLLATE utf8mb4_uni
 USE smartlib;
 
 -- Tabel admin (login)
-CREATE TABLE admin (
+CREATE TABLE IF NOT EXISTS admin (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
--- Masukkan satu user default (password: admin123)
+-- Masukkan admin default (password: admin123)
 INSERT INTO admin (username, password)
-VALUES ('admin', SHA2('admin123',256));
+VALUES ('admin', SHA2('admin123', 256));
 
 -- Tabel kategori
-CREATE TABLE kategori (
+CREATE TABLE IF NOT EXISTS kategori (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nama VARCHAR(100) NOT NULL
 );
 
 -- Tabel anggota
-CREATE TABLE anggota (
+CREATE TABLE IF NOT EXISTS anggota (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nama VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
@@ -28,17 +28,17 @@ CREATE TABLE anggota (
 );
 
 -- Tabel buku
-CREATE TABLE buku (
+CREATE TABLE IF NOT EXISTS buku (
   id INT AUTO_INCREMENT PRIMARY KEY,
   judul VARCHAR(200) NOT NULL,
   penulis VARCHAR(100) NOT NULL,
   kategori_id INT NOT NULL,
-  status ENUM('available','borrowed') NOT NULL DEFAULT 'available',
+  status ENUM('tersedia','dipinjam') NOT NULL DEFAULT 'tersedia',
   FOREIGN KEY (kategori_id) REFERENCES kategori(id) ON DELETE CASCADE
 );
 
 -- Tabel peminjaman
-CREATE TABLE peminjaman (
+CREATE TABLE IF NOT EXISTS peminjaman (
   id INT AUTO_INCREMENT PRIMARY KEY,
   anggota_id INT NOT NULL,
   buku_id INT NOT NULL,
@@ -49,10 +49,13 @@ CREATE TABLE peminjaman (
 );
 
 -- Tabel denda
-CREATE TABLE denda (
+CREATE TABLE IF NOT EXISTS denda (
   id INT AUTO_INCREMENT PRIMARY KEY,
   peminjaman_id INT NOT NULL,
   jumlah DECIMAL(10,2) NOT NULL,
   status ENUM('unpaid','paid') NOT NULL DEFAULT 'unpaid',
   FOREIGN KEY (peminjaman_id) REFERENCES peminjaman(id) ON DELETE CASCADE
 );
+
+--Tambah kolom gambar pada tabel buku
+ALTER TABLE buku ADD gambar VARCHAR(255);
